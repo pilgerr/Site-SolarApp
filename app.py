bd30f22745ce9d3be69960a8fd02dd35c2929e56
pilgerr <pilgerlucas2005@gmail.com>
2024-12-07 20:19:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def index():
 def generate_graphs():
     Icc = float(request.form['icc'])
     Kt = float(request.form['kt'])
-    V_values = np.linspace(0, 0.8, 100)
+    V_values = np.linspace(0, 0.8, 120)
 
     # Lista para armazenar os caminhos dos gráficos gerados
     graph_paths = []
