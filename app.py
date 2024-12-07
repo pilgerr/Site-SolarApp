eb6ae6a2921fec0bb799d0c7d058e208bf166761
pilgerr <pilgerlucas2005@gmail.com>
2024-12-07 20:33:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,8 @@
-from flask import Flask, render_template, request, send_from_directory
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Configuração da aplicação Flask
-app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/graphs'
-
-# Certifique-se de que o diretório para salvar gráficos existe
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-# Constantes
-k = 1.38e-23
-q = 1.602e-19
-n = 1.4
-Isr = 1.2799e-8
-Vg = 1.79e-19
-Tc = 298.15
-G_ref = 1000
-T_var = [298.15, 308.15, 318.15, 328.15, 338.15]
-G_var = [400, 600, 800, 1000, 1200]
-G = 800
-
-def corrente_fotovoltaica(V, Icc, Kt, G, G_ref, T, Tc):
-    Iph = ((Icc / G_ref) * G) * (1 + Kt * (T - Tc))
-    Is = Isr * (T / Tc) ** (3 / n) * np.exp((-(q * Vg) / (n * k)) * ((1 / T) - (1 / Tc)))
-    I = Iph - Is * np.exp(((q * V) / (n * k * T)) - 1)
-    return I
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/generate_graphs', methods=['POST'])
 def generate_graphs():
     Icc = float(request.form['icc'])
     Kt = float(request.form['kt'])
-    V_values = np.linspace(0, 0.8, 100)
+    V_values = np.linspace(0, 1.2, 100)  # Aumentei o intervalo para 1.2
 
     # Lista para armazenar os caminhos dos gráficos gerados
     graph_paths = []
@@ -51,6 +17,7 @@ def generate_graphs():
     plt.ylabel("Corrente (A)")
     plt.legend()
     plt.grid(True)
+    plt.xlim([0, 1.2])  # Garante que o gráfico vai até 1.2 no eixo X
     iv_temp_path = os.path.join(app.config['UPLOAD_FOLDER'], "iv_temp_graph.png")
     plt.savefig(iv_temp_path)
     graph_paths.append(iv_temp_path)
@@ -67,6 +34,7 @@ def generate_graphs():
     plt.ylabel("Potência (W)")
     plt.legend()
     plt.grid(True)
+    plt.xlim([0, 1.2])  # Garante que o gráfico vai até 1.2 no eixo X
     pv_temp_path = os.path.join(app.config['UPLOAD_FOLDER'], "pv_temp_graph.png")
     plt.savefig(pv_temp_path)
     graph_paths.append(pv_temp_path)
@@ -82,6 +50,7 @@ def generate_graphs():
     plt.ylabel("Corrente (A)")
     plt.legend()
     plt.grid(True)
+    plt.xlim([0, 1.2])  # Garante que o gráfico vai até 1.2 no eixo X
     iv_irra_path = os.path.join(app.config['UPLOAD_FOLDER'], "iv_irra_graph.png")
     plt.savefig(iv_irra_path)
     graph_paths.append(iv_irra_path)
@@ -98,16 +67,10 @@ def generate_graphs():
     plt.ylabel("Potência (W)")
     plt.legend()
     plt.grid(True)
+    plt.xlim([0, 1.2])  # Garante que o gráfico vai até 1.2 no eixo X
     pv_irra_path = os.path.join(app.config['UPLOAD_FOLDER'], "pv_irra_graph.png")
     plt.savefig(pv_irra_path)
     graph_paths.append(pv_irra_path)
     plt.close()
 
     return render_template('index.html', graph_paths=graph_paths)
-
-@app.route('/static/graphs/<filename>')
-def send_graph(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-if __name__ == '__main__':
-    app.run(debug=True)
